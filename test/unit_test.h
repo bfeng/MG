a51784f35f3455a0548e91f514cb44ba734f1f8f
Bo Feng <bfeng1216@gmail.com>
2012-11-12 02:26:58 -0600

    add a unit test header

---
+++ test/unit_test.h
@@ -0,0 +1,4 @@
+ #define assert(message, test) do { if (!(test)) return message; } while (0)
+ #define run_test(test) do { char *message = test(); tests_run++;  if (message) return message; } while (0)
+ extern int tests_run;
+
