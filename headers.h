a7e71aae74c30698af2d3be7f884e9c8d37e4b48
swang <will.sizhou.wang@gmail.com>
2012-11-20 13:25:10 -0600

    que and job data struct

--- headers.h
+++ headers.h
@@ -1,1 +1,23 @@
 #include "sys_info.h"
+
+
+
+//Data types
+//To keep consistency with CUDA code
+struct JobDescription{
+  int JobID;
+  int JobType;
+  int numThreads;
+  void *params;
+};
+typedef struct JobDescription *JobPointer; 
+
+struct QueueRecord {
+  struct JobDescription *Array; 
+  int Capacity;                 
+  int Rear;
+  int Front;
+  int ReadLock;
+};
+typedef struct QueueRecord *Queue;
+
